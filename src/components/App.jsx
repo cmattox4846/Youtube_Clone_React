8375bffd89fcc5be6d0ff1145a8e16004dde0fe6
cmattox4846 <cmattox4846@gmail.com>
2021-11-10 12:57:39 -0500

    added searchBar api call

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
+import axios from "axios";
+import SearchBar from './SearchBar/SearchBar';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            videos: []
+        };
     }
 
+  
+    SearchForVideo = async (search_term) => {
+        
+        let response = await axios.get('https://www.googleapis.com/youtube/v3/search?q='+{search_term}+'&key=AIzaSyASgHWeo95jh3pLZ-QrTEq_lx-OZCLXwGQ')
+        this.setState({
+            songs:response.data
+        })
+        
+    }
 
 
     render() {
         return (
-            <h1> TEST </h1>
-        )
+            <div>
+            <SearchBar search_term={this.SearchForVideo}/>
+            
 
+            <h1> TEST </h1>
+            </div>
+        );
+
+        }
     }
-}
+
 
 export default App;
