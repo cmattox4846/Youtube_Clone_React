dccd0413aeea2191660622ca090a5147230758ca
KasebrotII <kasebrot@live.com>
2021-11-15 09:44:29 -0600

    pulled update

--- src/components/CommentsList/CommentsList.jsx
+++ src/components/CommentsList/CommentsList.jsx
@@ -26,23 +26,21 @@ const CommentsList = (props) => {
               {props.comments.map((comment) => (
                 <tr key={comment.id}>
                   <td>{comment.comment}</td>
-                        <tr>
-                            {props.replies.map((reply, index) => {
-                                if(comment.id === reply.comment){
-                            return(
-                                <tr key={`${comment.id}-${index}`}>
-                                    <td className="replies">{reply.comment_reply}</td>
-                                </tr>
-                                )
-                             } else {
-                            return null
-                            }
-                            })}
-                        </tr>
-            
+                  <td>
+                      {props.replies.map((reply, index) => {
+                      if(comment.id === reply.comment){
+                        return(
+                          <tr key={`${comment.id}-${index}`} className="replies">
+                              <td>{reply.comment_reply}</td>
+                          </tr>
+                        )
+                      } else {
+                          return null
+                      }
+                      })}
+                  </td>            
                   <td>
                     {comment.likes}
-
                     <button type="button" className="btn btn-success btn-sm ms-2 me-2" onClick={() => props.addLike(comment.id)}> Like </button>
                   </td>
                   <td>
